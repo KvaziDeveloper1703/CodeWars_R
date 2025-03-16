483f11ab69777224dc7d3682483ec7efd4cf5d22
Viktor <viktor.sbruev.1999@gmail.com>
2025-03-16 02:14:45 +0200

    Added CW_7_6

---
+++ level_7/CW_7_6.r
@@ -0,0 +1,30 @@
+# You are given a non-empty string. Your task is to return its middle character(s).
+#If the string's length is odd, return the middle character.
+#If the string's length is even, return the middle two characters.
+
+#Example:
+#"test" → "es"
+
+#Вам дана непустая строка. Ваша задача — вернуть её средний символ(ы).
+#Если длина строки нечётная, вернуть один средний символ.
+#Если длина строки чётная, вернуть два средних символа.
+
+#Пример:
+#"test" → "es"
+
+
+get_middle_character <- function(given_string) {
+  length <- nchar(given_string)
+  middle <- length %/% 2
+
+  if (length %% 2 == 1) {
+    return(substr(given_string, middle + 1, middle + 1))
+  } else {
+    return(substr(given_string, middle, middle + 1))
+  }
+}
+
+print(get_middle_character("hello"))
+print(get_middle_character("world"))
+print(get_middle_character("test"))
+print(get_middle_character("middle"))
