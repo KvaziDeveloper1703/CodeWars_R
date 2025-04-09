cdccfd96459fe5d41ac6eaa8d5f54aedf5922868
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 21:53:53 +0300

    Modified CW_8_10

--- level_8/CW_8_10.r
+++ level_8/CW_8_10.r
@@ -1,16 +1,27 @@
-#Write a function that takes two parameters: an integer n and a string s. The function should return a new string where s is repeated exactly n times.
+# Write a function that takes two parameters:
+# an integer n and a string s.
+# The function should return a new string where s is repeated exactly n times.
 
-#Examples:
-#Input: 6, "I" → Output: "IIIIII"
-#Input: 5, "Hello" → Output: "HelloHelloHelloHelloHello"
+# Напишите функцию, которая принимает два параметра:
+# целое число n и строку s.
+# Функция должна вернуть новую строку, в которой s повторяется ровно n раз.
 
-#Напишите функцию, которая принимает два параметра: целое число n и строку s. Функция должна вернуть новую строку, в которой s повторяется ровно n раз.
-
-#Примеры:
-#Вход: 6, "I" → Выход: "IIIIII"
-#Вход: 5, "Hello" → Выход: "HelloHelloHelloHelloHello"
+# https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
 
 repeat_string <- function(n, s) {
   result <- strrep(s, n)
   return(result)
+}
+
+test_cases <- list(
+  list(n = 3, s = "abc"),
+  list(n = 5, s = "hello"),
+  list(n = 0, s = "test"),
+)
+
+for (test_case in test_cases) {
+  result <- repeat_string(test_case$n, test_case$s)
+  print(paste("Repeat string:", test_case$s,
+              "for", test_case$n,
+              "times:", result))
 }
