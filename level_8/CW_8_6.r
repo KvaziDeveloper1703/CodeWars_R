7a4a0a059c3c0d4b426b85c2db2cc42876bc9842
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 21:31:09 +0300

    Modified CW_8_6

--- level_8/CW_8_6.r
+++ level_8/CW_8_6.r
@@ -1,11 +1,24 @@
-#Write a function that takes a single boolean argument and returns "Yes" if the input is TRUE, or "No" if the input is FALSE.
+# Write a function that takes a single boolean argument and returns
+# "Yes" if the input is TRUE,
+# or "No" if the input is FALSE.
 
-#Напишите функцию, которая принимает один логический аргумент и возвращает строку "Yes", если входное значение равно TRUE, или строку "No", если входное значение равно FALSE.
+# Напишите функцию, которая принимает один логический аргумент и возвращает
+# строку "Yes", если входное значение равно TRUE,
+# или строку "No", если входное значение равно FALSE.
 
-bool_to_string <- function(bool) {
-  if (bool) {
+# https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+bool_to_string <- function(given_bool) {
+  if (given_bool) {
     return("Yes")
   } else {
     return("No")
   }
+}
+
+test_booleans <- c(TRUE, FALSE)
+
+for (test_bool in test_booleans) {
+  result <- bool_to_string(test_bool)
+  print(paste("Original boolean:", test_bool, "Converted to string:", result))
 }
