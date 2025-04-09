862342c45c98e38cd1dc7b29211f3af2ea9cee6a
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 22:43:28 +0300

    Modified CW_7_4

--- level_7/CW_7_4.r
+++ level_7/CW_7_4.r
@@ -1,14 +1,22 @@
-#Create a function that takes an integer as input, squares each of its digits, and concatenates the results to form a new number. The function should return this new number as an integer.
+# Create a function that takes an integer as input,
+# squares each of its digits,
+# and concatenates the results to form a new number.
+# The function should return this new number as an integer.
 
-#For example:
-#Input: 9119 → Output: 811181
-#Input: 765 → Output: 493625
+# For example:
+# Input: 765
+# Output: 493625
 
-#Создайте функцию, которая принимает целое число, возводит в квадрат каждую его цифру и объединяет результаты в новое число. Функция должна возвращать это новое число как целое число.
+# Создайте функцию, которая принимает целое число,
+# возводит в квадрат каждую его цифру
+# и объединяет результаты в новое число.
+# Функция должна возвращать это новое число как целое число.
 
-#Пример:
-#Вход: 9119 → Выход: 811181
-#Вход: 765 → Выход: 493625
+# Пример:
+# Вход: 765
+# Выход: 493625
+
+# https://www.codewars.com/kata/546e2562b03326a88e000020
 
 square_and_concatenate <- function(input_number) {
   digits <- strsplit(as.character(input_number), "")[[1]]
@@ -23,10 +31,6 @@ square_and_concatenate <- function(input_number) {
   return(result)
 }
 
-input1 <- 9119
-output1 <- square_and_concatenate(input1)
-print(output1)
-
-input2 <- 765
-output2 <- square_and_concatenate(input2)
-print(output2)
+input <- 765
+output <- square_and_concatenate(input)
+print(output)
