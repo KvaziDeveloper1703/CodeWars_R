80ad70ef989535baf2fe7e982c7cd717ba20399e
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 22:01:20 +0300

    Modified CW_7_1

--- level_7/CW_7_1.r
+++ level_7/CW_7_1.r
@@ -1,6 +1,13 @@
-#Write a function that counts the number of vowels in a given string. For this task, consider the letters "a", "e", "i", "o","u" and "y" as vowels. The input string will contain only lowercase letters and spaces.
+# Write a function that counts the number of vowels in a given string.
+# For this task, consider the letters "a", "e", "i", "o","u" and "y" as vowels.
+# The input string will contain only lowercase letters and spaces.
 
-#Напишите функцию, которая подсчитывает количество гласных букв в заданной строке. Для этой задачи учитываются только буквы "a", "e", "i", "o", "u" и "y". Входная строка будет состоять только из строчных букв и пробелов.
+# Напишите функцию, которая подсчитывает количество гласных букв
+# в заданной строке.
+# Для этой задачи учитываются только буквы "a", "e", "i", "o", "u" и "y".
+# Входная строка будет состоять только из строчных букв и пробелов.
+
+# https://www.codewars.com/kata/54ff3102c1bad923760001f3
 
 count_vowels <- function(input_string) {
   vowels <- c("a", "e", "i", "o", "u", "y")
@@ -8,11 +15,11 @@ count_vowels <- function(input_string) {
   characters <- strsplit(input_string, NULL)[[1]]
 
   vowel_count <- sum(characters %in% vowels)
-  
+
   return(vowel_count)
 }
 
-input_string <- "hello my friend"
+input_string <- "python is amazing"
 result <- count_vowels(input_string)
 
 print(paste("Number of vowels:", result))
