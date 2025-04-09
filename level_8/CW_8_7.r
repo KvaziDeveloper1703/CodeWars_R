de71d977046db45523578b872121c4950b1a8c8f
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 21:35:31 +0300

    Modified CW_8_7

--- level_8/CW_8_7.r
+++ level_8/CW_8_7.r
@@ -1,9 +1,18 @@
-#Write a function that takes a list of numbers, squares each number, and then returns the sum of these squares.
+# Write a function that takes a list of numbers,
+# squares each number, and then returns the sum of these squares.
 
-#Напишите функцию, которая принимает список чисел, возводит каждое из них в квадрат, а затем возвращает сумму этих квадратов.
+# Напишите функцию, которая принимает список чисел,
+# возводит каждое из них в квадрат, а затем возвращает сумму этих квадратов.
 
-sum_of_squares <- function(numbers) {
-  squared_numbers <- numbers^2
+# https://www.codewars.com/kata/515e271a311df0350d00000f
+
+sum_of_squares <- function(given_numbers) {
+  squared_numbers <- given_numbers^2
   result <- sum(squared_numbers)
   return(result)
 }
+
+test_numbers <- c(1, 2, 3, 4, 5)
+
+result <- sum_of_squares(test_numbers)
+print(paste("The sum of squares of the numbers is:", result))
