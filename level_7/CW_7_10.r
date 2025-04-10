e1d91d4f3b07283a8061e2f45e18723832d27fc9
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-10 14:26:51 +0300

    Modified CW_7_10

--- level_7/CW_7_10.r
+++ level_7/CW_7_10.r
@@ -1,12 +1,14 @@
-#Write a function that takes a string of words and returns the length of the shortest word(s).
+# Write a function that takes a string of words
+# and returns the length of the shortest word(s).
+# + The string will never be empty;
+# + You do not need to handle different data types.
 
-#The string will never be empty.
-#You do not need to handle different data types.
+# Напишите функцию, которая принимает строку из слов
+# и возвращает длину самого короткого слова.
+# + Строка не бывает пустой;
+# + Обрабатывать другие типы данных не нужно.
 
-#Напишите функцию, которая принимает строку из слов и возвращает длину самого короткого слова.
-
-#трока не бывает пустой.
-#Обрабатывать другие типы данных не нужно.
+# https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
 
 find_shortest_word_length <- function(given_string) {
   words <- unlist(strsplit(given_string, " "))
@@ -15,5 +17,5 @@ find_shortest_word_length <- function(given_string) {
   return(min(word_lengths))
 }
 
-print(find_shortest_word_length("The quick brown fox"))       # 3
-print(find_shortest_word_length("Jump over the lazy dog"))   # 3
+print(find_shortest_word_length("The quick brown fox"))
+print(find_shortest_word_length("Jump over the lazy dog"))
